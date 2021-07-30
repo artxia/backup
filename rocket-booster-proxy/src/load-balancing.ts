f50dbb2ec1112a38baae5ed00ca756c06faffefa
GitHub Action <action@github.com>
2021-07-30 04:07:08 +0000

    Action Update

--- rocket-booster-proxy/src/load-balancing.ts
+++ rocket-booster-proxy/src/load-balancing.ts
@@ -1,14 +1,22 @@
 import { Middleware } from '../types/middleware';
 
+const ipToNum = (
+  ip: string,
+) :number => ip.split('.').map((octect, index, array) => parseInt(octect, 10) * (256 ** (array.length - index - 1))).reduce((accumulator, current) => accumulator + current);
+
 export const useSelectUpstream: Middleware = (
   context,
   next,
 ) => {
   const { options } = context;
   const upstreamOptions = options.upstream;
-
   const upstream = Array.isArray(upstreamOptions) ? upstreamOptions : [upstreamOptions];
-  context.upstream = upstream[Math.floor(Math.random() * upstream.length)];
-
+  const ipString = context.request.headers.get('cf-connecting-ip');
+  if (ipString === null) {
+    context.upstream = upstream[Math.floor(Math.random() * upstream.length)];
+  } else {
+    const userIP = ipToNum(ipString);
+    context.upstream = upstream[userIP % upstream.length];
+  }
   return next();
 };
