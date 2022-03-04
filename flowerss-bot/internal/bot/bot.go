cd3c136b2dbd97a14e4e203dd9e156716b90fe76
GitHub Action <action@github.com>
2022-03-04 04:12:12 +0000

    Action Update

--- flowerss-bot/internal/bot/bot.go
+++ flowerss-bot/internal/bot/bot.go
@@ -3,7 +3,6 @@ package bot
 import (
 	"time"
 
-	"github.com/indes/flowerss-bot/internal/bot/fsm"
 	"github.com/indes/flowerss-bot/internal/bot/handler"
 	"github.com/indes/flowerss-bot/internal/bot/middleware"
 	"github.com/indes/flowerss-bot/internal/config"
@@ -14,9 +13,6 @@ import (
 )
 
 var (
-	// UserState 用户状态，用于标示当前用户操作所在状态
-	UserState map[int64]fsm.UserStatus = make(map[int64]fsm.UserStatus)
-
 	// B bot
 	B *tb.Bot
 )
@@ -25,38 +21,27 @@ func init() {
 	if config.RunMode == config.TestMode {
 		return
 	}
-	poller := &tb.LongPoller{Timeout: 10 * time.Second}
-	spamProtected := tb.NewMiddlewarePoller(
-		poller, func(upd *tb.Update) bool {
-			if !isUserAllowed(upd) {
-				// 检查用户是否可以使用bot
-				return false
-			}
-
-			if !CheckAdmin(upd) {
-				return false
-			}
-			return true
-		},
-	)
 	zap.S().Infow(
 		"init telegram bot",
 		"token", config.BotToken,
 		"endpoint", config.TelegramEndpoint,
 	)
-
 	// create bot
 	var err error
 	B, err = tb.NewBot(
 		tb.Settings{
 			URL:     config.TelegramEndpoint,
 			Token:   config.BotToken,
-			Poller:  spamProtected,
+			Poller:  &tb.LongPoller{Timeout: 10 * time.Second},
 			Client:  util.HttpClient,
-			Verbose: false,
+			Verbose: true,
 		},
 	)
-	B.Use(middleware.PreLoadMentionChat(), middleware.IsChatAdmin())
+	B.Use(
+		middleware.UserFilter(),
+		middleware.PreLoadMentionChat(),
+		middleware.IsChatAdmin(),
+	)
 	if err != nil {
 		zap.S().Fatal(err)
 		return
@@ -77,21 +62,21 @@ func Start() {
 func setCommands() {
 	commandHandlers := []handler.CommandHandler{
 		handler.NewStart(),
-		handler.NewVersion(),
 		handler.NewPing(B),
-		handler.NewRemoveSubscription(B),
-		handler.NewHelp(),
-		handler.NewExport(),
-		handler.NewImport(),
 		handler.NewAddSubscription(),
+		handler.NewRemoveSubscription(B),
 		handler.NewListSubscription(),
 		handler.NewRemoveAllSubscription(),
 		handler.NewOnDocument(B),
-		handler.NewPauseAll(),
-		handler.NewActiveAll(),
+		handler.NewSet(B),
 		handler.NewSetFeedTag(),
 		handler.NewSetUpdateInterval(),
-		handler.NewSet(B),
+		handler.NewExport(),
+		handler.NewImport(),
+		handler.NewPauseAll(),
+		handler.NewActiveAll(),
+		handler.NewHelp(),
+		handler.NewVersion(),
 	}
 
 	for _, h := range commandHandlers {
@@ -99,9 +84,14 @@ func setCommands() {
 	}
 
 	ButtonHandlers := []handler.ButtonHandler{
-		&handler.RemoveAllSubscriptionButton{},
-		&handler.CancelRemoveAllSubscriptionButton{},
+		handler.NewRemoveAllSubscriptionButton(),
+		handler.NewCancelRemoveAllSubscriptionButton(),
 		handler.NewSetFeedItemButton(B),
+		handler.NewRemoveSubscriptionItemButton(),
+		handler.NewNotificationSwitchButton(B),
+		handler.NewSetSubscriptionTagButton(B),
+		handler.NewTelegraphSwitchButton(B),
+		handler.NewSubscriptionSwitchButton(B),
 	}
 
 	for _, h := range ButtonHandlers {
@@ -119,10 +109,4 @@ func setCommands() {
 	if err := B.SetCommands(commands); err != nil {
 		zap.S().Errorw("set bot commands failed", "error", err.Error())
 	}
-
-	B.Handle(&tb.InlineButton{Unique: "set_toggle_notice_btn"}, setToggleNoticeBtnCtr)
-	B.Handle(&tb.InlineButton{Unique: "set_toggle_telegraph_btn"}, setToggleTelegraphBtnCtr)
-	B.Handle(&tb.InlineButton{Unique: "set_toggle_update_btn"}, setToggleUpdateBtnCtr)
-	B.Handle(&tb.InlineButton{Unique: "set_set_sub_tag_btn"}, setSubTagBtnCtr)
-	B.Handle(&tb.InlineButton{Unique: "unsub_feed_item_btn"}, unsubFeedItemBtnCtr)
 }
