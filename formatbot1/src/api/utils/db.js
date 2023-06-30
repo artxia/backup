41ff7fed0a97c5f414c9a2f37c6f3c86cd8051ff
GitHub Action <action@github.com>
2023-06-30 04:19:55 +0000

    Action Update

--- formatbot1/src/api/utils/db.js
+++ formatbot1/src/api/utils/db.js
@@ -321,7 +321,7 @@ const getCleanData = async () => {
       },
     },
   ];
-  const result = await linksOld1.aggregate(agg);
+  const result = await links.aggregate(agg);
   // const result2 = await checkTimeFromLast();
   return result.map(i => `${i._id.replace(/\./g, '_')} ${i.cnt}`);
 };
