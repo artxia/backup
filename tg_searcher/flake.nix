f6cf7183e5650b794b2cf454bb9777e89e7b69cc
GitHub Action <action@github.com>
2023-12-01 04:10:59 +0000

    Action Update

--- tg_searcher/flake.nix
+++ tg_searcher/flake.nix
@@ -2,32 +2,53 @@
   description = "Tg searcher: a searcher framework for Telegram";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    let
+      overlay = final: prev: {
+        python3 = prev.python3.override {
+          packageOverrides = pfinal: pprev: {
+            telethon = pprev.telethon.overridePythonAttrs (oldAttrs: rec {
+              version = "1.32.1";
+              src = final.fetchFromGitHub {
+                owner = "LonamiWebs";
+                repo = "Telethon";
+                rev = "refs/tags/v${version}";
+                hash = "sha256-0477SxYRVqRnCDPsu+q9zxejCnKVj+qa5DmH0VHuJyI=";
+              };
+              doCheck = false;
+            });
+
+            tg-searcher = pfinal.callPackage ./nix/searcher-pkg.nix { };
+          };
+        };
+        tg-searcher = final.python3Packages.tg-searcher;
+        python3Packages = final.python3.pkgs;
+      };
+    in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-          searcher-pkg = pkgs.callPackage ./nix/searcher-pkg.nix { };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
+          tg-searcher = pkgs.tg-searcher;
         in
         {
-          devShell = pkgs.mkShell {
-            buildInputs = [ searcher-pkg ];
-          };
-
-          defaultApp = flake-utils.lib.mkApp { drv = searcher-pkg; };
-          defaultPackage = searcher-pkg;
+          packages.default = tg-searcher;
+          legacyPackages = pkgs;
+          devShells.default = tg-searcher;
+          apps.default = flake-utils.lib.mkApp { drv = tg-searcher; };
         }
       )
     // {
-      overlays.default = final: prev: {
-        tg-searcher = self.defaultPackage.${prev.system};
-      };
+      overlays.default = overlay;
       nixosModules.default = {
-        nixpkgs.overlays = [ self.overlays.default ];
+        nixpkgs.overlays = [ overlay ];
         imports = [ ./nix/searcher-service.nix ];
       };
     };
